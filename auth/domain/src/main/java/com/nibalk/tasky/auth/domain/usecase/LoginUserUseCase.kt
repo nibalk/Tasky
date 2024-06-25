c1494724a1d1a7c0a3a9bbd0b72d73c099555ff5
Nisansala Bandara <nisansala.bandara@gmail.com>
2024-06-25 14:12:23 +0800

    Add domain layer classes for user login flow

--- auth/domain/src/main/java/com/nibalk/tasky/auth/domain/usecase/LoginUserUseCase.kt
+++ auth/domain/src/main/java/com/nibalk/tasky/auth/domain/usecase/LoginUserUseCase.kt
@@ -1,16 +1,16 @@
 package com.nibalk.tasky.auth.domain.usecase
 
 import com.nibalk.tasky.auth.domain.AuthRepository
-import com.nibalk.tasky.auth.domain.model.RegisterRequestParams
+import com.nibalk.tasky.auth.domain.model.LoginRequestParams
 import com.nibalk.tasky.core.domain.util.DataError
 import com.nibalk.tasky.core.domain.util.Result
 
-class RegisterUserUseCase(
+class LoginUserUseCase(
     private val authRepository: AuthRepository
 ) {
     suspend operator fun invoke(
-        registerParams: RegisterRequestParams
+        loginRequestParams: LoginRequestParams
     ) : Result<Unit, DataError.Network> {
-        return authRepository.register(registerParams)
+        return authRepository.login(loginRequestParams)
     }
 }
