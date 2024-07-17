8837e506152e343d40ee095de986acb8783e9125
Nisansala Bandara <nisansala.bandara@gmail.com>
2024-07-17 14:18:58 +0800

    Refactor to handle details state based on item type

--- agenda/presentation/src/main/java/com/nibalk/tasky/agenda/presentation/detail/DetailScreen.kt
+++ agenda/presentation/src/main/java/com/nibalk/tasky/agenda/presentation/detail/DetailScreen.kt
@@ -24,6 +24,7 @@ import com.nibalk.tasky.core.presentation.themes.TaskyTheme
 import com.nibalk.tasky.test.mock.AgendaSampleData
 import org.koin.androidx.compose.koinViewModel
 import org.koin.core.parameter.parametersOf
+import java.time.LocalDateTime
 
 @Composable
 fun DetailScreenRoot(
@@ -41,7 +42,7 @@ fun DetailScreenRoot(
                 else -> Unit
             }
             viewModel.onAction(action)
-        }
+        },
     )
 }
 
@@ -117,7 +118,11 @@ fun DetailScreen(
             TaskyEditableDateTimeRow(
                 label = stringResource(R.string.agenda_item_to),
                 isEditable = state.isEditingMode,
-                selectedDateTime = state.endDate.atTime(state.endTime),
+                selectedDateTime = state.getEventDetailField(
+                    state.details
+                ) { event ->
+                    event.endDate.atTime(event.endTime)
+                } ?: LocalDateTime.now(),
                 onTimeSelected = { selectedTime ->
                     onAction(DetailAction.OnEndTimeSelected(selectedTime))
                 },
@@ -130,7 +135,9 @@ fun DetailScreen(
         AgendaNotificationsRow(
             isEditable = state.isEditingMode,
             currentType = state.notificationDurationType,
-            onMenuItemClicked = {}
+            onMenuItemClicked = { notificationDurationType ->
+                onAction(DetailAction.OnNotificationDurationClicked(notificationDurationType))
+            }
         )
     }
 }
@@ -142,12 +149,16 @@ private fun DetailScreenPreview() {
         DetailScreen(
             state = DetailState(
                 isEditingMode = false,
-                agendaType = AgendaType.TASK,
-                agendaId = AgendaSampleData.task2.id,
-                title = AgendaSampleData.task2.title,
-                description = AgendaSampleData.task2.description,
-                startDate = AgendaSampleData.task2.startAt.toLocalDate(),
-                startTime = AgendaSampleData.task2.startAt.toLocalTime(),
+                agendaType = AgendaType.EVENT,
+                agendaId = AgendaSampleData.event2.id,
+                title = AgendaSampleData.event2.title,
+                description = AgendaSampleData.event2.description,
+                startDate = AgendaSampleData.event2.startAt.toLocalDate(),
+                startTime = AgendaSampleData.event2.startAt.toLocalTime(),
+                details = AgendaItemDetails.Event(
+                    endDate = AgendaSampleData.event2.endAt.toLocalDate(),
+                    endTime = AgendaSampleData.event2.endAt.toLocalTime(),
+                )
             ),
             onAction = {},
         )
--- agenda/presentation/src/main/java/com/nibalk/tasky/agenda/presentation/detail/DetailState.kt
+++ agenda/presentation/src/main/java/com/nibalk/tasky/agenda/presentation/detail/DetailState.kt
@@ -2,6 +2,7 @@ package com.nibalk.tasky.agenda.presentation.detail
 
 import com.nibalk.tasky.agenda.presentation.model.AgendaType
 import com.nibalk.tasky.agenda.presentation.model.NotificationDurationType
+import timber.log.Timber
 import java.time.LocalDate
 import java.time.LocalTime
 
@@ -16,7 +17,34 @@ data class DetailState(
     val description: String = "",
     val startDate: LocalDate = LocalDate.now(),
     val startTime: LocalTime = LocalTime.now(),
-    val endDate: LocalDate = LocalDate.now(),
-    val endTime: LocalTime = LocalTime.now(),
+    val details: AgendaItemDetails,
     val notificationDurationType: NotificationDurationType = NotificationDurationType.THIRTY_MINUTES,
-)
+) {
+    fun <T> getEventDetailField(
+        agendaItemDetails: AgendaItemDetails?,
+        fieldExtractor: (AgendaItemDetails.Event) -> T
+    ): T? {
+        Timber.d("getEventDetailField1 = %s", agendaItemDetails)
+        return if (agendaItemDetails is AgendaItemDetails.Event) {
+            val field = fieldExtractor(agendaItemDetails)
+            Timber.d("getEventDetailField2 = %s", field)
+            return field
+        } else {
+            null
+        }
+    }
+}
+
+sealed interface AgendaItemDetails {
+    data object NoDetails : AgendaItemDetails
+    data class Event(
+        val endDate: LocalDate = LocalDate.now(),
+        val endTime: LocalTime = LocalTime.now(),
+    ): AgendaItemDetails
+
+    data class Task(
+        val isDone: Boolean = false
+    ): AgendaItemDetails
+
+    data object Reminder: AgendaItemDetails
+}
