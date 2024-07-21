678de1e518f6ce26cf3b39683c1897a9d06d62d2
Nisansala Bandara <nisansala.bandara@gmail.com>
2024-07-22 04:18:22 +0800

    Add dao classes for task, event and reminders

---
+++ agenda/data/src/main/java/com/nibalk/tasky/agenda/data/local/dao/EventDao.kt
@@ -0,0 +1,32 @@
+package com.nibalk.tasky.agenda.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Transaction
+import androidx.room.Upsert
+import com.nibalk.tasky.agenda.data.local.entity.EventEntity
+import com.nibalk.tasky.agenda.data.local.entity.EventEntityFull
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface EventDao {
+    @Upsert
+    suspend fun upsertEvent(event: EventEntity)
+
+    @Upsert
+    suspend fun updateEvents(events: List<EventEntity>)
+
+    @Query("SELECT * FROM ${EventEntity.TABLE_NAME} WHERE ${EventEntity.PRIMARY_KEY} = :id")
+    suspend fun getEventById(id: String): EventEntityFull?
+
+    @Transaction
+    @Query("SELECT * FROM ${EventEntity.TABLE_NAME}")
+    fun getAllEvents(): Flow<List<EventEntityFull>>
+
+    @Delete
+    suspend fun deleteEvent(event: EventEntity)
+
+    @Query("DELETE FROM ${EventEntity.TABLE_NAME}")
+    suspend fun deleteAllEvents()
+}
